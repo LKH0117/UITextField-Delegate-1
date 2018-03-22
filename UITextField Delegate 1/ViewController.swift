1845cdaaf9b0741c52c29790387209fba14717b4
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-03-22 11:49:51 +0900

    pressed

--- UITextField Delegate 1/ViewController.swift
+++ UITextField Delegate 1/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var Hello: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TextField.placeholder = "입력해보세요!"
+        TextField.clearButtonMode = .whileEditing
     }
-
+    
+    @IBAction func Click(_ sender: Any) {
+        Hello.text = "Hello " + TextField.text!
+        TextField.text = ""
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
