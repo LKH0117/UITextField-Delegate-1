04249525789e21e4f5f22703afb2837ee74fc422
이경훈 | KyoungHun Lee <37130323+LKH0117@users.noreply.github.com>
2018-03-28 12:43:48 +0900

    Update ViewController.swift

--- UITextField Delegate 1/ViewController.swift
+++ UITextField Delegate 1/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var TextField: UITextField!
     @IBOutlet weak var Hello: UILabel!
@@ -16,13 +16,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Delegate 와 ViewController 연결한다.
+        txtField.delegate = self
+        
         TextField.placeholder = "입력해보세요!"
-        TextField.clearButtonMode = .whileEditing
+        TextField.clearButtonMode = UITextFieldViewMode.whileEditing
     }
     
     @IBAction func Click(_ sender: Any) {
         Hello.text = "Hello " + TextField.text!
         TextField.text = ""
+        txtField.resignFirstResponder()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        txtField.resignFirstResponder()
+        view.backgroundColor = UIColor.green
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txtField.resignFirstResponder()
+        view.backgroundColor = UIColor.red
+        return true
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
